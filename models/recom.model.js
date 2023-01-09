99d83a1c4692b6081d1ca8db69dd89c888c40ef4
Rudolf Teibtner <rudolfteibtner@mailbox.org>
2023-01-09 17:01:00 +0100

    add file model recommendation

---
+++ models/recom.model.js
@@ -0,0 +1,30 @@
+// const { Schema, model } = require("mongoose");
+
+// const recommendationSchema = new Schema(
+//   {
+//     title: {
+//       type: String,
+//       required: true,
+//     },
+//     serviceType: {
+//       type: String,
+//       enum: ["Job", "Housing"],
+//     },
+//     description: {
+//       type: String,
+//       required: true,
+//     },
+//     country: String,
+//     city: String,
+//     image: {
+//       type: String,
+//       default: "../public/images/default-img.png",
+//     },
+//     creator: String,
+//   },
+//   {
+//     timestamps: true,
+//   }
+// );
+
+// module.exports = model("Recom", recommendationSchema);
